3199b84239678cc18380ad7d9e43568669afe726
Amit <amit5yadav5762@gmail.com>
2025-03-28 13:04:17 +0530

    adding necessary comments

--- variables.js
+++ variables.js
@@ -2,4 +2,6 @@ let name = "AMit"
 let password = 232
 const finalPass = 3434
 
+// nowday we are not using this var keyword because of block and functional scope
+
 console.table([name,password,finalPass])
