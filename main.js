0194d09c0f036abc99a2ad0fbead818097e618be
Amit <amit5yadav5762@gmail.com>
2025-03-28 12:58:38 +0530

    Initial commit

---
+++ main.js
@@ -0,0 +1,21 @@
+let a =1;
+a=2;
+console.log(a);
+
+// second
+
+let firstName = "Amit";
+let age = 20;
+let job = false;
+
+console.log("My name is "+ firstName + " my age is " + age + "about job " + job);
+if(age>20){
+    console.log("approved");
+}
+else{
+    console.log("NOt approved");
+}
+
+for(let i=0;i<5;i++){
+    console.log("Hello");
+}
---
+++ variables.js
@@ -0,0 +1,5 @@
+let name = "AMit"
+let password = 232
+const finalPass = 3434
+
+console.table([name,password,finalPass])
