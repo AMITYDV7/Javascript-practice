bd1bce07f6fea9ddf2fab87b0b3713dd6efc953a
Amit <amit5yadav5762@gmail.com>
2025-03-29 14:41:58 +0530

    practing if else and loops

---
+++ Datatype.js
@@ -0,0 +1,24 @@
+"use strict";
+
+console.log("amit");
+
+
+
+let num = true
+
+if(num ==!true){
+    console.log("number is even")
+}
+else {
+    console.log("ODD")
+    
+}
+
+// question for practice
+
+let first_name="Amit"
+let last_name = "Yadav"
+
+console.log(first_name+" "+last_name)
+
+
